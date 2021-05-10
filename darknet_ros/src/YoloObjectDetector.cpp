fed7dc8174632098998535c662bcf3f705a6afbe
xucj <975114697@qq.com>
2021-05-10 21:41:25 +0800

    Fix bug for yolov4.

--- darknet_ros/src/YoloObjectDetector.cpp
+++ darknet_ros/src/YoloObjectDetector.cpp
@@ -8,6 +8,7 @@
 
 // yolo object detector
 #include "darknet_ros/YoloObjectDetector.hpp"
+#include "../../darknet/src/blas.h"
 
 // Check for xServer
 #include <X11/Xlib.h>
@@ -286,7 +287,7 @@ detection* YoloObjectDetector::avgPredictions(network* net, int* nboxes) {
       count += l.outputs;
     }
   }
-  detection* dets = get_network_boxes(net, buff_[0].w, buff_[0].h, demoThresh_, demoHier_, 0, 1, nboxes);
+  detection* dets = get_network_boxes(net, buff_[0].w, buff_[0].h, demoThresh_, demoHier_, 0, 1, nboxes, 1);
   return dets;
 }
 
@@ -296,7 +297,7 @@ void* YoloObjectDetector::detectInThread() {
 
   layer l = net_->layers[net_->n - 1];
   float* X = buffLetter_[(buffIndex_ + 2) % 3].data;
-  float* prediction = network_predict(net_, X);
+  float* prediction = network_predict(*net_, X);
 
   rememberNetwork(net_);
   detection* dets = 0;
@@ -312,7 +313,7 @@ void* YoloObjectDetector::detectInThread() {
     printf("Objects:\n\n");
   }
   image display = buff_[(buffIndex_ + 2) % 3];
-  draw_detections(display, dets, nboxes, demoThresh_, demoNames_, demoAlphabet_, demoClasses_);
+  draw_detections_v3(display, dets, nboxes, demoThresh_, demoNames_, demoAlphabet_, demoClasses_, 1);
 
   // extract the bounding boxes and send them to ROS
   int i, j;
@@ -380,7 +381,8 @@ void* YoloObjectDetector::fetchInThread() {
 }
 
 void* YoloObjectDetector::displayInThread(void* ptr) {
-  int c = show_image(buff_[(buffIndex_ + 1) % 3], "YOLO", 1);
+  show_image(buff_[(buffIndex_ + 1) % 3], "YOLO");
+  int c = -1;
   if (c != -1) c = c % 256;
   if (c == 27) {
     demoDone_ = 1;
